c45c63f9d8838d40faa9ee7ef4d37b2ac78149ba
Jeffry Gonzalez <jeff@hypertheory.com>
2022-03-03 16:25:06 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+
+app.MapGet("/", () =>
+{
+    var response = new DeveloperInfo("Nick", "Cave", "nick@aol.com", "867-5309");
+    return response;
+})
+.WithName("GetOnCallDeveloper");
+
+app.Run();
+
+
+public record DeveloperInfo(string firstName, string lastName, string emailAddress, string telephoneNumber);
